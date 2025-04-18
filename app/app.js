c7d40a40c522ce99fda713bf017328f997c92745
gyamficodes <gyamfijohn08@gmail.com>
2025-04-18 13:07:50 +0000

    small intro about the maening of Data Structure && Agorithem.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 
-
+//Data Structure
 const StudentDatabase = ['Yaw', 'Kwame', 'Ama', 'John', 'Kofi']
 
+
+//Agorithem for finding a specific user.
 const foundStuden = (allStudent, studentName) => {
        for(let i = 0; i < allStudent.length; i++){
         if(allStudent[i] === studentName){
@@ -9,5 +11,23 @@ const foundStuden = (allStudent, studentName) => {
         }
        }
 }
-
 foundStuden(StudentDatabase, 'Kwame')
+
+//Data Structure
+const StudentAgesDatabase = [20, 50, 60 , 70];
+
+//Agorithem for finding a specific age.
+const handdleStudentAge = (allAges, singleAge) => {
+    for(let i = 0; i < allAges.length; i++){
+        if(allAges[i] === singleAge){
+            console.log('found', `${singleAge}`)
+        }
+    }
+}
+
+handdleStudentAge(StudentAgesDatabase, 60);
+
+
+
+
+
