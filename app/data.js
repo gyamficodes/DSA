f95ac2479c9978c5d328a8c5c3fa68082a092275
gyamficodes <gyamfijohn08@gmail.com>
2025-09-09 08:46:55 +0000

    Frequency Counter Method

--- app/data.js
+++ app/data.js
@@ -1,2 +1,21 @@
-const name = "DSA";
-console.log(`Hello, ${name}!`);
+// How To Analyze Data Structures and Algorithms Problems
+
+// 1 Time Complexity
+// 2 Space Complexity
+// 3 Auxiliary Space Complexity
+// N/W
+//CPU 
+
+
+// Frequency Counter Method
+
+
+//Agorithem to sum(A, B)
+
+const sumAge = (a, n) => {
+    let s = 0;
+    for(let i = 0; i< n; i++){
+        s = s + a[i]
+        return s;
+    }
+} 
