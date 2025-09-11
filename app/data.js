450b001c7d4666c03a24a04c1ccf15e46eb1b96e
gyamficodes <gyamfijohn08@gmail.com>
2025-09-11 23:25:24 +0000

    learning time ad space

--- app/data.js
+++ app/data.js
@@ -13,11 +13,22 @@
 //Agorithem to sum(A, B)
 
 
-const sumAge = (a, n) => {
-    let s = 0;
-    for(let i = 0; i < n; i++){
-        s = s + a[i]
-        return s;
-    }
+const sumAge = (a, n) => {                       //space
+    let s = 0;                // 1                    a - 1
+    for(let i = 0; i < n; i++){  // n + 1             n - 1
+     s =   s + a[i]          // n                      i  -1
+    }                                                  
+    return s               // 1
 } 
+
+//Time  f(n) = 2n + 3 => O(n)   
+//Space  S(n) = 3 => O(1)
+
+const MyAge = [3, 10, 18, 20, 25, 30, 35, 40, 45, 50];
+console.log(sumAge(MyAge, MyAge.length));  // 276
+
+
+
+
+
 
