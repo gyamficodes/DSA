6ce2e9e13521543f4886470ffa70046039378ad0
gyamficodes <gyamfijohn08@gmail.com>
2025-09-12 22:59:46 +0000

    diving deep into time function

--- app/data.js
+++ app/data.js
@@ -28,6 +28,25 @@ const MyAge = [3, 10, 18, 20, 25, 30, 35, 40, 45, 50];
 console.log(sumAge(MyAge, MyAge.length));  // 276
 
 
+let a = 0; // 1
+ while(a <= 10){  // n + 1               space
+    console.log(a); // n                  a - 1
+    a++ // n                              n - 1
+ }
+
+ // Time  f(n) = 3n + 2 => O(n)
+ // Space  S(n) = 1 => O(1)
+
+
+//Types of time functions
+//1 Constant Time O(1)
+//2 Logarithmic Time O(log n)
+//3 Linear Time O(n)
+//4 Linearithmic Time O(n log n)
+//5 Quadratic Time O(n^2)
+//6 Exponential Time O(2^n)
+//7 Factorial Time O(n!)
+
 
 
 
